94ad865a558708b9f4b10de858092059dbd0ebbc
Nathan <noodlescodes@gmail.com>
2014-05-11 19:42:54 +1000

    updated the parsing to output nicely.

--- src/ExParser.java
+++ src/ExParser.java
@@ -1,5 +1,3 @@
-// Requires the solution to be in a different format to what the program will output currently. Going to figure out a better way to parse it.
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -10,19 +8,25 @@ import java.io.PrintWriter;
 
 public class ExParser {
 	public static void main(String[] args) {
-		String file = "ExSolutionCompressed13.dat";
+		String file = "ExSolution.dat";
 		BufferedReader br;
 		String line;
-		int[][] sol = new int[13][13];
+		int[][] sol = new int[43][43];
 
 		try {
 			br = new BufferedReader(new FileReader(file));
 			while ((line = br.readLine()) != null) {
-				int i = Integer.parseInt(line.split("\\s")[0].split(":")[0]) - 1;
-				int j = Integer.parseInt(line.split("\\s")[0].split(":")[1]) - 1;
-				// String test = line.split("\\s")[1];
-				sol[i][j] = Integer.parseInt(line.split("\\s")[1]);
-				System.out.println("");
+				if (line.length() > 0) {
+					if (line.charAt(0) == 'B') {
+						line = line.replaceAll("\\s+", "");
+						line = line.substring(1);
+						int i = Integer.parseInt(line.split("E")[0].split(",")[0]) - 1;
+						int j = Integer.parseInt(line.split("E")[0].split(",")[1]) - 1;
+						// String test = line.split("\\s")[1];
+						sol[i][j] = Integer.parseInt(line.split("E")[1]);
+						System.out.println("");
+					}
+				}
 			}
 		} catch (FileNotFoundException e) {
 		} catch (IOException e) {
@@ -32,29 +36,27 @@ public class ExParser {
 			for (int j = 0; j < sol.length; j++) {
 				if (j + 1 < sol.length) {
 					System.out.print(sol[i][j] + ",");
-				}
-				else {
+				} else {
 					System.out.print(sol[i][j]);
 				}
 			}
 			System.out.println("");
 		}
-		
+
 		try {
 			PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter("ExSolution13CSV.csv")));
 			for (int i = 0; i < sol.length; i++) {
 				for (int j = 0; j < sol.length; j++) {
 					if (j + 1 < sol.length) {
 						w.print(sol[i][j] + ",");
-					}
-					else {
+					} else {
 						w.print(sol[i][j]);
 					}
 				}
 				w.println("");
 			}
 			w.close();
-		} catch(IOException e) {
+		} catch (IOException e) {
 		}
 	}
 }
